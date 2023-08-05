a58a45cadc692ff5c74cd554f07da370fb146531
Goddchen <goddchen@gmail.com>
2023-08-05 14:53:54 +0200

    feat: init (#1)
    * feat: init

---
+++ test/dummy_test.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:goddchen_cv/main.dart';
+
+void main() {
+  testWidgets('Dummy, remove once real tests are here',
+      (final WidgetTester widgetTester) async {
+    await widgetTester.pumpWidget(const MainApp());
+  });
+}
