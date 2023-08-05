a20d84715e97ff75a748cd4d1e4b1eabda7ab77a
Goddchen <goddchen@gmail.com>
2023-08-05 15:47:48 +0200

    feat: use flutter_flavorizr (#2)
    * feat: use flutter_flavorizr

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,31 @@
+import 'dart:async';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:goddchen_cv/flavors.dart';
 
-void main() {
-  runApp(const MainApp());
+FutureOr<void> main() async {
+  runApp(const App());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class App extends StatelessWidget {
+  const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    final Scaffold scaffold = Scaffold(
+      appBar: AppBar(
+        title: Text(F.title),
       ),
+    );
+    return MaterialApp(
+      title: F.title,
+      home: F.appFlavor == Flavor.develop || kDebugMode
+          ? Banner(
+              message: F.name,
+              location: BannerLocation.topStart,
+              child: scaffold,
+            )
+          : scaffold,
     );
   }
 }
