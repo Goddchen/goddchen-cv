816f594dd1b345125d73fda88bed0f6f6e016900
Goddchen <goddchen@gmail.com>
2023-08-06 19:07:35 +0200

    fix: fixes #13, scrolling issue on mobile

--- lib/github_pr/github_pr_view.dart
+++ lib/github_pr/github_pr_view.dart
@@ -14,33 +14,45 @@ class GithubPrView extends MvcView<GithubPrModel, GithubPrController> {
 
   @override
   Widget build(final BuildContext context) => Card(
-        child: SizedBox(
-          height: 160,
-          child: Padding(
-            padding: const EdgeInsets.all(16),
-            child: Center(
-              child: model.pr.build(
-                dataBuilder: (final GithubPrModelPr data) => Row(
-                  children: <Widget>[
-                    Assets.icons.github.pullRequest.svg(
-                      height: 48,
-                      width: 48,
-                    ),
-                    const SizedBox(width: 16),
-                    Expanded(
-                      child: Column(
-                        mainAxisSize: MainAxisSize.min,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(16),
+          child: Material(
+            type: MaterialType.transparency,
+            child: InkWell(
+              onTap: () {},
+              child: SizedBox(
+                height: 160,
+                child: Padding(
+                  padding: const EdgeInsets.all(16),
+                  child: Center(
+                    child: model.pr.build(
+                      dataBuilder: (final GithubPrModelPr data) => Row(
                         children: <Widget>[
-                          Text('${data.link}'),
-                          Text(data.title),
-                          Text('${data.createdAt}'),
+                          Assets.icons.github.pullRequest.svg(
+                            height: 48,
+                            width: 48,
+                          ),
+                          const SizedBox(width: 16),
+                          Expanded(
+                            child: Column(
+                              mainAxisSize: MainAxisSize.min,
+                              children: <Widget>[
+                                Text('${data.link}'),
+                                Text(data.title),
+                                Text('${data.createdAt}'),
+                              ],
+                            ),
+                          ),
                         ],
                       ),
+                      errorBuilder: (final Object error) => ErrorWidget(error),
+                      loadingBuilder: () => const CircularProgressIndicator(),
                     ),
-                  ],
+                  ),
                 ),
-                errorBuilder: (final Object error) => ErrorWidget(error),
-                loadingBuilder: () => const CircularProgressIndicator(),
               ),
             ),
           ),
