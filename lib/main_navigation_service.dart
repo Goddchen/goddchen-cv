12cbe9c4371b5378a9c2522337407ecc867c7998
Goddchen <goddchen@gmail.com>
2023-08-08 07:55:09 +0200

    feat: close drawer on item tap (#29)
    resolves #27

---
+++ lib/main_navigation_service.dart
@@ -0,0 +1,4 @@
+abstract interface class MainNavigationService {
+  void pop();
+  bool canPop();
+}
--- lib/services/navigation/navigation_service.dart
+++ lib/services/navigation/navigation_service.dart
@@ -1,4 +1,8 @@
+import 'package:flutter/widgets.dart';
+import 'package:fpdart/fpdart.dart';
+import 'package:goddchen_cv/constants.dart';
 import 'package:goddchen_cv/github_prs/github_prs_navigation_service.dart';
+import 'package:goddchen_cv/main_navigation_service.dart';
 import 'package:goddchen_cv/portfolio/portfolio_navigation_service.dart';
 import 'package:goddchen_cv/youtube_videos/youtube_videos_navigation_service.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
@@ -13,13 +17,26 @@ NavigationServiceAggregator navigationService(final _) =>
 abstract interface class NavigationServiceAggregator
     implements
         GithubPrsNavigationService,
+        MainNavigationService,
         PortfolioNavigationService,
         YoutubeVideosNavigationService {}
 
 class NavigationServiceImplementation implements NavigationServiceAggregator {
   @override
+  bool canPop() => optionOf(navigatorKey.currentState).fold(
+        () => false,
+        (final NavigatorState navigatorState) => navigatorState.canPop(),
+      );
+
+  @override
   void openUri({required final Uri uri}) => launchUrl(
         uri,
         mode: LaunchMode.externalApplication,
+      );
+
+  @override
+  void pop() => optionOf(navigatorKey.currentState).fold(
+        () {},
+        (final NavigatorState navigatorState) => navigatorState.pop(),
       );
 }
