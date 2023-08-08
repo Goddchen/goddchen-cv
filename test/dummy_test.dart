b8c2c415876dd1fc415d07fbacebf85a3b425648
Goddchen <goddchen@gmail.com>
2023-08-08 15:56:41 +0200

    test: add Youtube tests (#30)

--- test/dummy_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:goddchen_cv/main.dart';
-
-void main() {
-  testWidgets('Dummy, remove once real tests are here',
-      (final WidgetTester widgetTester) async {
-    await widgetTester.pumpWidget(const App());
-  });
-}
