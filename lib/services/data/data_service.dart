6730317f8ba026eb3aab4628c991da41458038aa
Goddchen <goddchen@gmail.com>
2024-09-22 17:50:11 +0200

    feat: add latest PR
    Signed-off-by: Goddchen <goddchen@gmail.com>

--- lib/services/data/data_service.dart
+++ lib/services/data/data_service.dart
@@ -325,6 +325,14 @@ class DataServiceImplementation implements DataServiceAggregator {
                 'Fix: popToNamed(...), need to break from the while loop when entry found',
             user: 'slovnicki',
           ),
+          GithubPrsDataServicePr(
+            link: Uri.parse(
+              'https://github.com/MaikuB/flutter_local_notifications/issues/2309',
+            ),
+            repo: 'flutter_local_notifications',
+            title: 'Exception while trying to read notification channels',
+            user: 'MaikuB',
+          ),
         ],
       );
 
